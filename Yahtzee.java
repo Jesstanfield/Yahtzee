ad1889749e37ddc67f6f6693a96fdc3f229f03c5
Armaan Aggarwal <armaan@Armaans-MacBook-Pro.local>
2020-02-20 15:05:11 -0600

    Tab Space Change

--- Yahtzee.java
+++ Yahtzee.java
@@ -1,71 +1,71 @@
 import java.util.*;
 
 public class Yahtzee {
-  static final int NUMBER_OF_DICE = 5;
-  static final int NUM_REROLLS = 2;
-  static int[] dice = new int[NUMBER_OF_DICE];
+    static final int NUMBER_OF_DICE = 5;
+    static final int NUM_REROLLS = 2;
+    static int[] dice = new int[NUMBER_OF_DICE];
 
-  public static void main(String[] args) {
-    Scanner keyboard = new Scanner(System.in);
-    rollDice();
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        rollDice();
 
-    for (int i = 0; i < NUM_REROLLS; i++) {
-      if (isYahtzee()) {
-        break;
-      }
-      System.out.println(diceToString());
-      System.out.print("Which dice do you want to reroll: ");
-      rollDice(convert(keyboard.nextLine()));
+        for (int i = 0; i < NUM_REROLLS; i++) {
+            if (isYahtzee()) {
+                break;
+            }
+            System.out.println(diceToString());
+            System.out.print("Which dice do you want to reroll: ");
+            rollDice(convert(keyboard.nextLine()));
+        }
+        System.out.println(diceToString());
+        if (isYahtzee()) {
+            System.out.println("You got Yahtzee!");
+        } else {
+            System.out.println("Sorry, better luck next time!");
+        }
     }
-    System.out.println(diceToString());
-    if (isYahtzee()) {
-      System.out.println("You got Yahtzee!");
-    } else {
-      System.out.println("Sorry, better luck next time!");
+
+    public static void rollDice(int[] diceToChange) {
+        for (int i : diceToChange) {
+            dice[i-1] = getRandomDieValue();
+        }
     }
-  }
 
-  public static void rollDice(int[] diceToChange) {
-    for (int i : diceToChange) {
-      dice[i-1] = getRandomDieValue();
+    public static void rollDice() {
+        for (int i = 0; i < NUMBER_OF_DICE; i++) {
+            dice[i] = getRandomDieValue();
+        }
     }
-  }
 
-  public static void rollDice() {
-    for (int i = 0; i < NUMBER_OF_DICE; i++) {
-      dice[i] = getRandomDieValue();
+    public static int getRandomDieValue() {
+        return (int) (Math.random() * 6 + 1);
     }
-  }
 
-  public static int getRandomDieValue() {
-    return (int) (Math.random() * 6 + 1);
-  }
+    public static int[] convert(String s) {
+        StringTokenizer st = new StringTokenizer(s);
+        int[] a = new int[st.countTokens()];
+        int i = 0;
 
-  public static int[] convert(String s) {
-    StringTokenizer st = new StringTokenizer(s);
-    int[] a = new int[st.countTokens()];
-    int i = 0;
-
-    while (st.hasMoreTokens()) {
-      a[i++] = Integer.parseInt(st.nextToken());
+        while (st.hasMoreTokens()) {
+            a[i++] = Integer.parseInt(st.nextToken());
+        }
+        return a;
     }
-    return a;
-  }
 
-  public static boolean isYahtzee() {
-    for (int el : dice) {
-      if (el != dice[0]) {
-        return false;
-      }
+    public static boolean isYahtzee() {
+        for (int el : dice) {
+            if (el != dice[0]) {
+                return false;
+            }
+        }
+        return true;
     }
-    return true;
-  }
 
-  public static String diceToString() {
-    String result = "Your dice are: ";
-    for (int el : dice) {
-      result += el + " ";
+    public static String diceToString() {
+        String result = "Your dice are: ";
+        for (int el : dice) {
+            result += el + " ";
+        }
+        return result;
     }
-    return result;
-  }
 }
